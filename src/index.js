5ba93011b67e05e80db74baece5b71b7a9d3e62b
Will Hurley <wjhurley@users.noreply.github.com>
2017-08-06 01:03:11 -0700

    adds redux, react-redux, and redux-thunk to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import './styles/normalize.css';
 import './styles/index.css';
 import App from './components/App';
+import reducers from './reducers';
 
 // Also configure store here
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
