96603791f7076a6d0a984bc0bff7219b928db321
Will Hurley <wjhurley@users.noreply.github.com>
2017-08-06 18:17:13 -0700

    sets up redux store

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,27 @@
+function addTodo(message) {
+  return {
+    type: 'ADD_TODO',
+    message: message,
+    completed: false
+  };
+}
 
+function completeTodo(index) {
+  return {
+    type: 'COMPLETE_TODO',
+    index: index
+  };
+}
+
+function deleteTodo(index) {
+  return {
+    type: 'DELETE_TODO',
+    index: index
+  };
+}
+
+function clearTodo() {
+  return {
+    type: 'CLEAR_TODO'
+  };
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import { todoComponent } from './items';
 
 export default combineReducers({
-
+  todoComponent
 });
