f7858d11502159af4425449d0ab9bc41860099c6
Will Hurley <wjhurley@users.noreply.github.com>
2017-09-17 03:07:58 -0700

    adds unsplash and dotenv dependencies for background image

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import axios from 'axios';
 
 import {fetchWeather} from "../actions/weatherActions";
 
@@ -15,7 +16,24 @@ import TodoWidget from './TodoWidget';
 
 import './App.css';
 
+
 class App extends Component {
+
+  async fetchImage() {
+    let image;
+    await axios.get("https://api.unsplash.com/collections/1194376/photos/?client_id=50bb0aa059c83c9c3338c46f60256f2c8d1a24952948a7b9903d2abf1e67a778")
+      .then(function(response) {
+        let rand = Math.floor(Math.random() * response.data.length),
+            arr = Array.from(response.data);
+        console.log(rand);
+        image = `url(${arr[rand].urls.raw})`;
+        console.log(image);
+        return image ? image : "url(http://i.imgur.com/HXyY3vl.jpg)";
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
   componentDidMount() {
     const {dispatch} = this.props; // passed by default with connect
@@ -24,7 +42,10 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div
+        className="App"
+        style={{backgroundImage: this.fetchImage()}}
+      >
         <LinksWidget/>
         <ZenWidget/>
         <WeatherWidget/>
